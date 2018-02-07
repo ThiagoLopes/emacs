42a89429fddbb76493ae22be8ca672de4fbb2607
ThiagoLopes <thiagolopes@protonmail.com>
2018-02-07 15:37:45 -0200

    update init!

--- init.el
+++ init.el
@@ -4,23 +4,50 @@
 
 ;;; Code:
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;;-----PACKAGES-----------------------------------------------------------------
+;; Package sources
 (package-initialize)
+(unless (assoc-default "melpa" package-archives)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t))
+(unless (assoc-default "melpa" package-archives)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t))
+(unless (assoc-default "melpa" package-archives)
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t))
+;;------------------------------------------------------------------------------
 
-(setq user-full-name "Thiago Lopes")
-(setq user-mail-address "thiagolopes@protonmail.com")
+;;Record startup timestamp
+(defvar super-emacs/invokation-time
+(current-time))
+
+(setq user-full-name "Thiago Lopes"
+      user-mail-address "thiagolopes@protonmail.com")
 
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
+;; (add-to-list 'load-path ".orgmode/")
+
+(setq gc-cons-threshold 64000000)
+(add-hook 'after-init-hook #'(lambda ()
+                               ;; restore after startup
+                               (setq gc-cons-threshold 800000)))
 
 (require 'base)
 (require 'base-keybindings)
 (require 'base-packages)
 (require 'base-themes)
-(require 'lang-javascript)
+;; (require 'lang-javascript)
 (require 'lang-python)
 (require 'lang-web)
+
+;;Print welcome message
+(princ (cl-concatenate 'string
+                       "01000101 01101101 01100001 01100011 01110011\n\n\n"
+                       "Startup completed in "
+                       (number-to-string (cadr (time-subtract (current-time)
+                                                              super-emacs/invokation-time)))
+                       " seconds\n\n"
+                       "Welcome to ｅｍａｃｓ  安ェ殴ど依挨虞!\n\n"
+                       "Today's date: "
+                       (format-time-string "%B %d %Y"))
+(get-buffer-create (current-buffer)))
 
 ;;; init.el ends here
