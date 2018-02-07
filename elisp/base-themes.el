ecd0a520dd876c3bcecdc95b3a067aa26e193640
ThiagoLopes <thiagolopes@protonmail.com>
2018-02-07 15:38:17 -0200

    change theme to dracula

--- elisp/base-themes.el
+++ elisp/base-themes.el
@@ -6,28 +6,55 @@
 
 ;; Monoaki Alt
 (use-package monokai-alt-theme
+  :defer t
   :disabled)
 
 ;; Aftermoon
 (use-package afternoon-theme
+  :defer t
   :disabled)
 
 ;; Monokai
 (use-package monokai-theme
+  :defer t
   :disabled)
 
 ;; Sanityinc
 (use-package color-theme-sanityinc-tomorrow
+  :defer t
   :disabled)
 
 ;; Spacemacs
 (use-package spacemacs-theme
   :defer t
   :init
+  :disabled
   (load-theme 'spacemacs-dark t))
 
-;; Change font
-(set-face-attribute 'default nil :family "Hack" :height 90)
+(use-package dracula-theme
+  :ensure t
+  :config
+  (load-theme 'dracula t))
 
-(provide 'base-themes)
+;; Change font
+;; (set-face-attribute 'default nil :family "Hack" :height 90)
+
+;; (use-package spacemacs-fonts-support
+;;   :config
+;;   (spacemacs/set-default-font
+;;    '("Hack"
+;;      :size 10
+;;      :weight normal
+;;      :width normal)))
+
+(use-package powerline
+  :config
+  (powerline-center-theme)
+  (setq powerline-default-separator
+        'slant))
+
+
+
+
+  (provide 'base-themes)
 ;;; themes.el ends here
