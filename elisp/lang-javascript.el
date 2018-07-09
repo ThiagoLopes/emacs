2d84460d306c71894729a36b0ea13bcc7e84a6c9
ThiagoLopes <thiagolopes@protonmail.com>
2018-07-09 12:42:52 -0300

    add javascript support

--- elisp/lang-javascript.el
+++ elisp/lang-javascript.el
@@ -1,24 +1,48 @@
-;; js2-mode
-;; https://github.com/mooz/js2-mode
-
+(use-package json-mode
+  :defer t)
 (use-package js2-mode
-  :commands js2-mode
-  :mode "\\.js?\\'"
-  :init
-  (progn
-    (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-    (setq-default js2-basic-offset 2)
-    (add-to-list 'interpreter-mode-alist (cons "node" 'js2-mode)))
+  :defer t
   :config
-  (progn
-    (js2-imenu-extras-setup)
-    (bind-key "C-x C-e" 'js-send-last-sexp js2-mode-map)
-    (bind-key "C-M-x" 'js-send-last-sexp-and-go js2-mode-map)
-    (bind-key "C-c b" 'js-send-buffer js2-mode-map)
-    (bind-key "C-c d" 'my/insert-or-flush-debug js2-mode-map)
-    (bind-key "C-c C-b" 'js-send-buffer-and-go js2-mode-map)
-    (bind-key "C-c w" 'my/copy-javascript-region-or-buffer js2-mode-map)))
+  ;; Disable js2 mode's syntax error highlighting by default...
+  (setq-default js2-mode-show-parse-errors nil
+                js2-mode-show-strict-warnings nil)
+  ;; ... but enable it if flycheck can't handle javascript
+  (autoload 'flycheck-get-checker-for-buffer "flycheck")
+  (defun sanityinc/disable-js2-checks-if-flycheck-active ()
+    (unless (flycheck-get-checker-for-buffer)
+      (set (make-local-variable 'js2-mode-show-parse-errors) t)
+      (set (make-local-variable 'js2-mode-show-strict-warnings) t)))
+  (add-hook 'js2-mode-hook 'sanityinc/disable-js2-checks-if-flycheck-active)
+  (setq-default js2-basic-offset 2 js2-bounce-indent-p nil)
+  (add-hook 'js2-mode-hook (lambda () (setq mode-name "JS2")))
+  (js2-imenu-extras-setup)
+  )
 
-(add-to-list 'auto-mode-alist '("\\.js\\'\\|\\.json\\'" . js2-mode))
+(use-package coffee-mode
+  :defer t)
+(use-package typescript-mode
+  :defer t)
+(use-package prettier-js
+  :defer t)
+
+(defcustom preferred-javascript-mode
+  :type 'symbol
+  :group 'programming
+  :options '(js2-mode js-mode))
+
+(defconst preferred-javascript-indent-level 2)
+
+;; Need to first remove from list if present, since elpa adds entries too, which
+;; may be in an arbitrary order
+(eval-when-compile (require 'cl))
+(setq auto-mode-alist (cons `("\\.\\(js\\|es6\\)\\(\\.erb\\)?\\'" . ,preferred-javascript-mode)
+                            (loop for entry in auto-mode-alist
+                                  unless (eq preferred-javascript-mode (cdr entry))
+                                  collect entry)))
+
+;; js-mode
+(setq-default js-indent-level preferred-javascript-indent-level)
+
+(add-to-list 'interpreter-mode-alist (cons "node" preferred-javascript-mode))
 
 (provide 'lang-javascript)
