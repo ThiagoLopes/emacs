572311097657845e9f629ac96556fad66f5f9896
ThiagoLopes <thiagolopes@protonmail.com>
2018-07-09 15:16:51 -0300

    create base-exec-path and base-utils

---
+++ elisp/base-exec-path.el
@@ -0,0 +1,10 @@
+;;; Code:
+
+(use-package exec-path-from-shell
+  :config
+  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
+    (add-to-list 'exec-path-from-shell-variables var)))
+
+
+(provide 'base-exec-path)
+;;; base-exec-path ends here
