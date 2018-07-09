6c62cc5a9d824f619b893351351eb77fc629c404
ThiagoLopes <thiagolopes@protonmail.com>
2018-07-09 15:54:48 -0300

    add reapply-themes

--- elisp/base-themes.el
+++ elisp/base-themes.el
@@ -7,13 +7,21 @@
 ;;------------------------------------------------------------------------------
 ;; Toggle between light and dark
 ;;------------------------------------------------------------------------------
+;; Ensure that themes will be applied even if they have not been customized
+(defun reapply-themes ()
+  "Forcibly load the themes listed in `custom-enabled-themes'."
+  (dolist (theme custom-enabled-themes)
+    (unless (custom-theme-p theme)
+      (load-theme theme)))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+
 (defun light ()
   "Activate a light color theme."
   (interactive)
   (setq custom-enabled-themes '(sanityinc-tomorrow-day))
   (reapply-themes))
 
-(defun dark ()
+(defun night ()
   "Activate a dark color theme."
   (interactive)
   (setq custom-enabled-themes '(sanityinc-tomorrow-bright))
