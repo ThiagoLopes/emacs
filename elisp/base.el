f35ca14ed8a5a54a50160a5613d34a16d79c3b41
ThiagoLopes <thiagolopes@protonmail.com>
2018-09-25 23:47:12 -0300

    refactoring init.el and base.el configs. leaving the basic settings simpler and clearer

--- elisp/base.el
+++ elisp/base.el
@@ -1,23 +1,17 @@
 ;;; package --- Main Base file
 ;;; Commentary:
-;;; Base com configurações básicas
+;;; Basic configs
 
 ;;; Code:
 ;; Change tab width and change tabs to spaces
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
 
-;; Set undo tree
-(setq undo-tree-history-directory-alist '(("." . "~/.emacs-backup/undo")))
-
-;; Set backups
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+;; Spell
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 
 ;; Disk space is cheap. Save lots.
 (setq delete-old-versions -1)
-(setq version-control t)
-(setq vc-make-backup-files t)
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
 
 ;; Save history command
 (setq savehist-file "~/.emacs.d/savehist")
@@ -30,8 +24,6 @@
         search-ring
         regexp-search-ring))
 
-
-;; Configurando UTF-8
 ;; UTF-8 please
 (set-charset-priority 'unicode)
 (setq locale-coding-system   'utf-8)   ; pretty
@@ -41,47 +33,51 @@
 (prefer-coding-system        'utf-8)   ; with sugar on top
 (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
 
-;; ;; Emacs customizations
+;; Disable blink cursor
+(blink-cursor-mode 0)
+
+;; Backup
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+(setq create-lockfiles nil)  ; stop creating .# files
+
+;; Variables
 (setq confirm-nonexistent-file-or-buffer  t
       save-interprogram-paste-before-kill t
-      mouse-yank-at-point                 t
+      mouse-yank-at-point                 nil
       require-final-newline               t
       visible-bell                        nil
       ring-bell-function                  'ignore
       custom-file                         "~/.emacs.d/.custom.el"
-      ;; http://ergoemacs.org/emacs/emacs_stop_cursor_enter_prompt.html
       minibuffer-prompt-properties
       '(read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)
-
-      ;; Disable non selected window highlight
-      cursor-in-non-selected-windows     nil
-      highlight-nonselected-windows      nil
-      ;; PATH
-      exec-path                          (append exec-path '("/usr/local/bin/"))
-      inhibit-startup-message            t
-      fringes-outside-margins            t
+      ;; highlight-nonselected-windows   t
+      ;; fringes-outside-margins          t
       x-select-enable-clipboard          t
       use-package-always-ensure          t)
 
+(setq inhibit-startup-message t         ; Don't show the startup message
+      inhibit-startup-screen t          ; or screen
+      cursor-in-non-selected-windows t  ; Hide the cursor in inactive windows
+      echo-keystrokes 0.1               ; Show keystrokes right away, don't show the message in the scratch buffe
+      initial-scratch-message nil       ; Empty scratch buffer
+      initial-major-mode 'org-mode      ; org mode by default
+      sentence-end-double-space nil     ; Sentences should end in one space, come on!
+      confirm-kill-emacs 'y-or-n-p      ; y and n instead of yes and no when quitting
+      help-window-select t              ; select help window so it's easy to quit it with 'q'
+)
 
-(setq history-length                     999
-      backup-inhibited                   nil
-      make-backup-files                  t
-      auto-save-default                  t
-      make-backup-files                  t
-      create-lockfiles                   nil)
 
-(fset 'yes-or-no-p 'y-or-n-p)
-(global-auto-revert-mode t)
-
-;; Disable toolbar & menubar
-(menu-bar-mode -1)
+;; UI
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)(menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq use-file-dialog nil)
-(setq use-dialog-box nil)
-(setq inhibit-startup-screen t)
-
+(fset 'yes-or-no-p 'y-or-n-p)      ; y and n instead of yes and no everywhere else
+(global-auto-revert-mode t)
+(delete-selection-mode 1)
+(global-unset-key (kbd "s-p"))
+(global-hl-line-mode nil)
 
 ;; Time in the modeline
 (display-time-mode 1)
@@ -94,39 +90,26 @@
 (setq-default show-trailing-whitespace t)
 
 ;; Show collumn number
-(column-number-mode 1)
-
-;; Setup `hippie-expand' expand functions
-(global-set-key (kbd "M-/") 'hippie-expand)
-(setq hippie-expand-try-functions-list '(try-expand-dabbrev
-                                         try-expand-dabbrev-all-buffers
-                                         try-expand-dabbrev-from-kill
-                                         try-complete-file-name-partially
-                                         try-complete-file-name
-                                         try-expand-all-abbrevs
-                                         try-expand-list
-                                         try-expand-line
-                                         try-complete-lisp-symbol-partially
-                                         try-complete-lisp-symbol))
-
-;; display the current column in mode-line
 (setq column-number-mode t)
 
-;; set default width to 100 columns
-(setq-default fill-column 79)
+;; Save and kill
+(global-set-key (kbd "s-q") 'save-buffers-kill-emacs) ;; quit
 
-(setq frame-title-format
-      "ｅｍａｃｓ  安ェ殴ど依挨虞")
+;; ;; Setup `hippie-expand' expand functions
+;; (global-set-key (kbd "M-/") 'hippie-expand)
+;; (setq hippie-expand-try-functions-list '(try-expand-dabbrev
+;;                                          try-expand-dabbrev-all-buffers
+;;                                          try-expand-dabbrev-from-kill
+;;                                          try-complete-file-name-partially
+;;                                          try-complete-file-name
+;;                                          try-expand-all-abbrevs
+;;                                          try-expand-list
+;;                                          try-expand-line
+;;                                          try-complete-lisp-symbol-partially
+;;                                          try-complete-lisp-symbol))
 
 ;;Enable show-paren-mode
 (show-paren-mode 1)
-
-;;Disable splash message, start *scratch* buffer by default
-;; (setq initial-buffer-choice
-;; t)
-;; (setq initial-scratch-message
-;; "")
-
 
 ;; Uniquify
 (require 'uniquify)
@@ -134,34 +117,6 @@
 (setq uniquify-separator " • ")
 (setq uniquify-after-kill-buffer-p t)
 (setq uniquify-ignore-buffers-re "^\\*")
-
-
-;; Spell if enabled
-(when *spell-check-support-enabled*
-  (require 'init-spelling))
-
-;; PATH
-;; (let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
-;;   (setenv "PATH" path)
-;;   (setq exec-path
-;;         (append
-;;          (split-string-and-unquote path ":")
-;;          exec-path)))
-
-
-;; Some term enhancement
-(defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
-  (if (memq (process-status proc) '(signal exit))
-      (let ((buffer (process-buffer proc)))
-        ad-do-it
-        (kill-buffer buffer))
-    ad-do-it))
-(ad-activate 'term-sentinel)
-
-(defadvice ansi-term (before force-bash)
-  (interactive (list "/bin/zsh")))
-(ad-activate 'ansi-term)
-
 
 (provide 'base)
 ;;; base ends here
--- init.el
+++ init.el
@@ -4,40 +4,29 @@
 
 ;;; Code:
 
-;;----------------------------------------------------------------------------
-;; Adjust garbage collection thresholds during startup, and thereafter
-;;----------------------------------------------------------------------------
-(let ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 128 1024 1024)))
-  (setq gc-cons-threshold init-gc-cons-threshold)
-  (add-hook 'after-init-hook
-            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
-
 ;;-----PACKAGES-----------------------------------------------------------------
 ;; Package sources
 (require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
-;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(eval-when-compile (require 'use-package))
+(eval-when-compile
+  (setq use-package-expand-minimally byte-compile-current-file))
+
 ;;------------------------------------------------------------------------------
 
-;;Record startup timestamp
-(defvar super-emacs/invokation-time
-  (current-time))
-
+;; Configs
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
-;; (add-to-list 'load-path ".orgmode/")
-
-;; Spell
-(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
-
 (require 'base)
 (require 'base-keybindings)
 (require 'base-packages)
@@ -50,16 +39,11 @@
 (require 'lang-go)
 
 ;;Print welcome message
-(princ (cl-concatenate 'string
-                       ";; 01000101 01101101 01100001 01100011 01110011\n\n\n"
-                       ";; Startup completed in "
-                       (number-to-string (cadr (time-subtract (current-time)
-                                                              super-emacs/invokation-time)))
-                       " seconds\n\n"
-                       ";; Welcome to ｅｍａｃｓ  安ェ殴ど依挨虞!\n\n"
-                       ";; Today's date: "
-                       (format-time-string "%B %d %Y")
-                       (concat "\n\n;; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
-       (get-buffer-create (current-buffer)))
+(cl-concatenate 'string
+                ";; Welcome to ｅｍａｃｓ"
+                ";; Today's date: "
+                (format-time-string "%B %d %Y")
+                (concat "\n\n;; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
+(get-buffer-create (current-buffer)))
 
 ;;; init.el ends here
