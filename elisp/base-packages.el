039316f7ca050862ce80891a84532e7f085f77b5
ThiagoLopes <thiagolopes@protonmail.com>
2018-12-06 18:12:44 -0200

    remove auto update

--- elisp/base-packages.el
+++ elisp/base-packages.el
@@ -179,8 +179,7 @@
 (use-package auto-package-update
   :ensure t
   :config
-  (setq auto-package-update-delete-old-versions t
-        auto-package-update-interval 4)
+  (setq auto-package-update-delete-old-versions t)
   (auto-package-update-maybe))
 
 (use-package yaml-mode
