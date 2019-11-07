ffa426b6e1dff994ba89ca1f88e84ef00282b3b1
thiagolopes <thiagolopes@protonmail.com>
2019-11-07 13:50:35 -0300

    update python config

--- lisp/base-python.el
+++ lisp/base-python.el
@@ -11,11 +11,17 @@
 
 (when (maybe-require-package 'anaconda-mode)
   (after-load 'python
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+    ;; Anaconda doesn't work on remote servers without some work, so
+    ;; by default we enable it only when working locally.
+    (add-hook 'python-mode-hook
+              (lambda () (unless (file-remote-p default-directory)
+                      (anaconda-mode 1))))
+    (add-hook 'anaconda-mode-hook 'anaconda-eldoc-mode)
     ;; HACK comment this to treat underscore as part of the word
     ;; (add-hook 'python-mode-hook #'(lambda () (modify-syntax-entry ?_ "w")))
     )
+  (after-load 'anaconda-mode
+    (define-key anaconda-mode-map (kbd "M-?") nil))
   (when (maybe-require-package 'company-anaconda)
     (after-load 'company
       (after-load 'python
