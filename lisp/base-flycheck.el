13d6b9f03ff0a571693b33863af12a5407666ff7
thiagolopes <thiagolopes@protonmail.com>
2019-11-12 18:44:20 -0300

    Flycheck only in save

--- lisp/base-flycheck.el
+++ lisp/base-flycheck.el
@@ -5,6 +5,7 @@
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+  (setq flycheck-check-syntax-automatically '(save))
 
   (when (maybe-require-package 'flycheck-color-mode-line)
     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
