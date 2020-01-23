1b6e33a661fa4c826618d82a17639d9821140975
thiagolopes <thiagolopes@protonmail.com>
2020-01-23 13:16:46 -0300

    revert font config

--- lisp/base-themes.el
+++ lisp/base-themes.el
@@ -64,7 +64,7 @@
 ;; Good fonts to code - Hack, Iosevka, Terminus, PragmataPro
 ;;------------------------------------------------------------------------------
 (condition-case nil
-    (set-face-attribute 'default nil :font "Terminus 10")
+    (set-face-attribute 'default nil :font "Hack 09")
   (error nil))
 
 ;;------------------------------------------------------------------------------
